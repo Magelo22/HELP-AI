ab9c0673452ce0ca1b48e4cefebd33470e8f050e
Nicolas Dayvison <nicolasdayvison2018@gmail.com>
2025-11-02 12:32:02 -0300

    sobre atualizado

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, } from '@angular/common/http';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    importProvidersFrom()
+  ]
+};
